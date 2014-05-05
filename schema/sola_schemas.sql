2b0d79e81fb671bd5548f889303b18532dbfb238
amcdowell <armcdowell@hotmail.com>
2014-05-06 09:58:06 +1200

    Update schema files

--- schema/sola_schemas.sql
+++ schema/sola_schemas.sql
@@ -1785,6 +1785,81 @@ COMMENT ON FUNCTION cadastre_object_name_is_valid(name_firstpart character varyi
 
 
 --
+-- Name: change_parcel_name(character varying, character varying, character varying, character varying); Type: FUNCTION; Schema: cadastre; Owner: postgres
+--
+
+CREATE FUNCTION change_parcel_name(parcel_id character varying, part1 character varying, part2 character varying, user_name character varying) RETURNS void
+    LANGUAGE plpgsql
+    AS $$  
+BEGIN
+
+    IF parcel_id IS NULL THEN
+	   RETURN; 
+	END IF;
+
+    -- First remove the LRS parcel of the same name if one exists. 
+	UPDATE cadastre.cadastre_object
+	SET   change_user = user_name
+	WHERE name_firstpart = part1
+	AND   name_lastpart =  part2
+	AND   source_reference = 'LRS';
+
+	UPDATE administrative.ba_unit_contains_spatial_unit 
+	SET change_user = user_name
+	WHERE spatial_unit_id IN (
+	   SELECT co.id
+	   FROM   cadastre.cadastre_object co
+	   WHERE name_firstpart = part1
+	   AND   name_lastpart =  part2
+	   AND   source_reference = 'LRS')
+	OR spatial_unit_id = parcel_id;
+
+	-- Disassociation the LRS parcel as well as the parcel that
+	-- will get the name change from any BA Units. 
+	DELETE FROM administrative.ba_unit_contains_spatial_unit 
+	WHERE spatial_unit_id IN (
+	   SELECT co.id
+	   FROM   cadastre.cadastre_object co
+	   WHERE name_firstpart = part1
+	   AND   name_lastpart =  part2
+	   AND   source_reference = 'LRS')
+	OR spatial_unit_id = parcel_id;
+
+	DELETE FROM cadastre.cadastre_object co
+	WHERE  co.name_firstpart = part1
+	AND    co.name_lastpart = part2
+	AND    co.source_reference = 'LRS';
+
+	-- Change the name of the parcel and link it to the ba_unit if a ba_unit
+	-- exists with a matching name. 
+	UPDATE cadastre.cadastre_object co
+	SET    name_firstpart = part1,
+	       name_lastpart = part2,
+		   change_user = user_name
+	WHERE  co.id = parcel_id;
+
+	INSERT INTO administrative.ba_unit_contains_spatial_unit (ba_unit_id, spatial_unit_id, change_user)
+	SELECT ba.id, co.id, user_name
+	FROM   administrative.ba_unit ba,
+		   cadastre.cadastre_object co
+	WHERE  ba.name_firstpart = regexp_replace(part1, '\D', '', 'g') -- Remove the LOT part from the name
+	AND    ba.name_lastpart = part2
+	AND    co.name_firstpart = ba.name_firstpart
+	AND    co.name_lastpart = ba.name_lastpart;
+   
+END; $$;
+
+
+ALTER FUNCTION cadastre.change_parcel_name(parcel_id character varying, part1 character varying, part2 character varying, user_name character varying) OWNER TO postgres;
+
+--
+-- Name: FUNCTION change_parcel_name(parcel_id character varying, part1 character varying, part2 character varying, user_name character varying); Type: COMMENT; Schema: cadastre; Owner: postgres
+--
+
+COMMENT ON FUNCTION change_parcel_name(parcel_id character varying, part1 character varying, part2 character varying, user_name character varying) IS 'Procedure to correct the name of a cadastre object that was incorrectly recorded in DCDB. During the LRS migration, non spatial LRS parcels were created. The procedure will remove any LRS parcel matching the new name as well as link the parcel to the BA Unit matching the new name.';
+
+
+--
 -- Name: f_for_tbl_cadastre_object_trg_geommodify(); Type: FUNCTION; Schema: cadastre; Owner: postgres
 --
 
