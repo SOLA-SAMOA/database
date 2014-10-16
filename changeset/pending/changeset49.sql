383528f7025323fdf1686ae4d243812ad767c24e
amcdowell <armcdowell@hotmail.com>
2014-10-16 16:55:36 +1300

    Added village Aopo [#163 responsible:none state:fixed]
    See Changeset49.sql

---
+++ changeset/pending/changeset49.sql
@@ -0,0 +1,5 @@
+-- 16 Oct 2014
+-- #163 Add Aopo Village
+INSERT INTO administrative.ba_unit (id, type_code, name, name_firstpart, name_lastpart, status_code, change_user)
+VALUES ('LOC501', 'administrativeUnit', 'Aopo/Village', 'Aopo', 'Village', 'current', 'andrew'); 
+
