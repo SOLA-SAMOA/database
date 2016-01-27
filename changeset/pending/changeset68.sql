730b0d8731131a34338a464d1d97deabb50a881b
amcdowell <armcdowell@hotmail.com>
2016-01-27 13:33:24 +1300

    Changeset68 - Add link from Atua District to Namu'a Village [#181 state:fixed responsible:none]

---
+++ changeset/pending/changeset68.sql
@@ -0,0 +1,5 @@
+-- 27 Jan 2016
+-- #181 Add Namu'a Village and link to district Atua
+INSERT INTO administrative.required_relationship_baunit(
+            from_ba_unit_id, to_ba_unit_id, relation_code, change_user)
+    VALUES ('LD715954','LOC502', 'district_Village', 'andrew');
